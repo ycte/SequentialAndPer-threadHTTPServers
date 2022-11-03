b180eca8c93d5f96e268c52bde4aabe96856ba8a
ycte <b15260056838@163.com>
2022-11-03 11:08:32 +0800

    add cgi

--- ProcessBuilderTest.java
+++ ProcessBuilderTest.java
@@ -1,27 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import jdk.nashorn.internal.runtime.Debug;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class ProcessBuilderTest {
-    public static void main(String[] args) {
-        try {
-            Process process = Runtime.getRuntime().exec("java -jar ProcessJar.jar args1 agrs2 args3");
-            InputStream is = process.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-
-            int exitCode = process.waitFor();
-            System.out.println(exitCode);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+    public static String runPB(String command, String directory, Map<String, String> env) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                command);
+//        System.out.println(env.keySet());
+        Set set = env.keySet();
+        Iterator iterator = set.iterator();
+        while (iterator.hasNext()){
+            Object key = iterator.next();
+            Object value = env.get(key);
+            processBuilder.environment().put((String) key, (String) value);
+            System.out.println("env add: "+key+"===>"+value);
         }
-//————————————————
-//        版权声明：本文为CSDN博主「朱小厮」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-//        原文链接：https://blog.csdn.net/u013256816/article/details/54603910
+        processBuilder.environment().put("QUERY_STRING", "/appl/");
+        processBuilder.directory(new File(directory));
+        Process process = processBuilder.start();
+//        OutputStreamWriter osw = new OutputStreamWriter(process.getOutputStream());
+//        osw.write("rake routes");
+//        osw.close();
+        System.out.print(printStream(process.getErrorStream()));
+        String result = printStream(process.getInputStream());
+        return result;
+    }
+
+    public static String printStream(InputStream is) throws IOException {
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String line, result = "";
+        while ((line = br.readLine()) != null) {
+//            System.out.println(line);
+            result += line;
+        }
+        return result;
+    }
+    public static void main(String[] args) throws IOException {
+//        try {
+//            Process process = Runtime.getRuntime().exec("./cgi/price.cgi");
+//            InputStream is = process.getInputStream();
+//            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+//
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+//
+//            int exitCode = process.waitFor();
+//            System.out.println(exitCode);
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+//        ProcessBuilder pb = new ProcessBuilder(args);
+        Map<String, String> env = new HashMap<>();
+//        Map<String, String> environment;
+//        if (environment != null)
+//            env.putAll(environment);
+//        Process process = pb.start();
+        env.put("QUERY_STRING", "/appl/");
+        env.put("a","b");
+        String result = runPB("./cgi/price.cgi","./", env);
+        System.out.println(result);
     }
 }
